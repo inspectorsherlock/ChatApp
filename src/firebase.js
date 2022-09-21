4783f1bb9f2cc9ebaa9775b3ae16d0b6128250e3
Fawwaz <fawwaz.chowdhury@googlemail.com>
2022-09-21 18:13:24 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
+//Left blank for security purposes
 const firebaseConfig = {
-    apiKey: "AIzaSyDvSFH7NA0hjsb5lfS1BvdVBeARbRv0ftU",
-    authDomain: "chat1-a30a8.firebaseapp.com",
-    projectId: "chat1-a30a8",
-    storageBucket: "chat1-a30a8.appspot.com",
-    messagingSenderId: "1049994435089",
-    appId: "1:1049994435089:web:8c895b84a306c64979afb0"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 
